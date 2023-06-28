1907853fd0b6b7a2bac6c39a1d266be202ad7e56
mattityui <m.pamaong58@gmail.com>
2023-06-28 12:39:27 +0800

    Update account.js

--- JS/account.js
+++ JS/account.js
@@ -1,105 +1,64 @@
-const shrink_btn = document.querySelector(".shrink-btn");
-const search = document.querySelector(".search");
-const sidebar_links = document.querySelectorAll(".sidebar-links a");
-const active_tab = document.querySelector(".active-tab");
-const shortcuts = document.querySelector(".sidebar-links h4");
-const tooltip_elements = document.querySelectorAll(".tooltip-element");
+const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
 
-let activeIndex;
+allSideMenu.forEach((item) => {
+  const li = item.parentElement;
 
-shrink_btn.addEventListener("click", () => {
-  document.body.classList.toggle("shrink");
-  setTimeout(moveActiveTab, 400);
-
-  shrink_btn.classList.add("hovered");
-
-  setTimeout(() => {
-    shrink_btn.classList.remove("hovered");
-  }, 500);
+  item.addEventListener("click", function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
 });
 
-search.addEventListener("click", () => {
-  document.body.classList.remove("shrink");
-  search.lastElementChild.focus();
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector("#content nav .bx.bx-menu");
+const sidebar = document.getElementById("sidebar");
+
+menuBar.addEventListener("click", function () {
+  sidebar.classList.toggle("hide");
 });
 
-function moveActiveTab() {
-  let topPosition = activeIndex * 58 + 2.5;
+const searchButton = document.querySelector(
+  "#content nav form .form-input button"
+);
+const searchButtonIcon = document.querySelector(
+  "#content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector("#content nav form");
 
-  if (activeIndex > 3) {
-    topPosition += shortcuts.clientHeight;
-  }
-
-  active_tab.style.top = `${topPosition}px`;
-}
-
-function changeLink() {
-  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
-  this.classList.add("active");
-
-  activeIndex = this.dataset.active;
-
-  moveActiveTab();
-}
-
-sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
-
-function showTooltip() {
-  let tooltip = this.parentNode.lastElementChild;
-  let spans = tooltip.children;
-  let tooltipIndex = this.dataset.tooltip;
-
-  Array.from(spans).forEach((sp) => sp.classList.remove("show"));
-  spans[tooltipIndex].classList.add("show");
-
-  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
-}
-
-tooltip_elements.forEach((elem) => {
-  elem.addEventListener("mouseover", showTooltip);
-});
-// Get the menu links and sections
-const menuLinks = document.querySelectorAll(".menu-link");
-const sections = document.querySelectorAll(".section");
-
-// Activate the first section by default
-sections[0].classList.add("active");
-
-// Add click event listener to each menu link
-menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener("click", (e) => {
+searchButton.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
     e.preventDefault();
-
-    // Get the section associated with the clicked link
-    const sectionId = menuLink.dataset.section;
-    const section = document.getElementById(sectionId);
-
-    // Toggle the active class for menu links
-    menuLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-    menuLink.classList.add("active");
-
-    // Toggle the active class for sections
-    sections.forEach((sec) => {
-      sec.classList.remove("active");
-      sec.style.display = "none";
-    });
-    section.classList.add("active");
-    section.style.display = "block";
-  });
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchButtonIcon.classList.replace("bx-search", "bx-x");
+    } else {
+      searchButtonIcon.classList.replace("bx-x", "bx-search");
+    }
+  }
 });
 
-// Hide sections that are not active
-function hideInactiveSections() {
-  sections.forEach((sec) => {
-    if (!sec.classList.contains("active")) {
-      sec.style.display = "none";
-    } else {
-      sec.style.display = "block";
-    }
-  });
+if (window.innerWidth < 768) {
+  sidebar.classList.add("hide");
+} else if (window.innerWidth > 576) {
+  searchButtonIcon.classList.replace("bx-x", "bx-search");
+  searchForm.classList.remove("show");
 }
 
-// Call the function to hide inactive sections initially
-hideInactiveSections();
+window.addEventListener("resize", function () {
+  if (this.innerWidth > 576) {
+    searchButtonIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
+});
+
+const switchMode = document.getElementById("switch-mode");
+
+switchMode.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
