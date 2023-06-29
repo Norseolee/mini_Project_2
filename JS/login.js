090211c8d5d301381266c7070c0f914acecbf7ba
mattityui <m.pamaong58@gmail.com>
2023-06-29 17:56:16 +0800

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -17,3 +17,49 @@ sign_up_btn2.addEventListener("click", () => {
 sign_in_btn2.addEventListener("click", () => {
   container.classList.remove("sign-up-mode2");
 });
+
+function addAccount(e) {
+  e.preventDefault();
+
+  var firstName = document.getElementById("fname").value;
+  var lastName = document.getElementById("lname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pass").value;
+
+  var user = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+  };
+
+  var json = JSON.stringify(user);
+  localStorage.setItem(email, json);
+
+  document.getElementById("sign-up-form").reset();
+  console.log("User added:", user);
+}
+
+var signUpForm = document.getElementById("sign-up-form");
+signUpForm.addEventListener("submit", addAccount);
+
+function validateLogin() {
+  event.preventDefault();
+
+  var formToReset = document.getElementById("sign-in-form");
+  var email = document.getElementById("login-email").value;
+  var password = document.getElementById("login-pass").value;
+
+  var user = localStorage.getItem(email);
+  var data = JSON.parse(user);
+  console.log(data);
+
+  if (user == null) {
+    console.log("wrong email or password");
+  } else if (email == data.email && password == data.password) {
+    console.log("Success!");
+    window.location.replace("../index.html");
+  } else {
+    console.log("wrong email or password");
+  }
+}
